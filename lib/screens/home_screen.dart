1e94d1981d1d1613b8805f3cfe2e65e38b962a54
Charlie Nielsen <54167446+HytroN@users.noreply.github.com>
2022-12-14 20:06:15 +0100

    ..

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -32,120 +32,112 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    return Column(
-      children: [
-        Container(
-          height: (mediaQuery.size.height) * 0.2,
-          // margin: EdgeInsets.all(25),
-          child: CircleAvatar(
-            backgroundColor: Colors.blue.shade300,
-            radius: 40,
+    return Scaffold(
+      floatingActionButton: ElevatedButton(
+        style: ElevatedButton.styleFrom(shape: null),
+        child: Row(
+          children: [
+            const Icon(Icons.add),
+            Text('Opret Rejsekort'),
+          ],
+        ),
+        onPressed: () => print('object'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Column(
+        children: [
+          Container(
+            height: (mediaQuery.size.height) * 0.2,
+            // margin: EdgeInsets.all(25),
             child: CircleAvatar(
-              radius: 37,
-              backgroundColor: Colors.white,
-              child: HeartBeat(
-                beatsPerMinute: 45,
-                child: FaIcon(
-                  FontAwesomeIcons.nfcSymbol,
-                  size: 40,
-                  color: Colors.blue.shade300,
+              backgroundColor: Colors.blue.shade300,
+              radius: 40,
+              child: CircleAvatar(
+                radius: 37,
+                backgroundColor: Colors.white,
+                child: HeartBeat(
+                  beatsPerMinute: 45,
+                  child: FaIcon(
+                    FontAwesomeIcons.nfcSymbol,
+                    size: 40,
+                    color: Colors.blue.shade300,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Container(
-          child: CarouselSlider(
-            options: CarouselOptions(
-              height: 170.0,
-              enlargeCenterPage: true,
-              autoPlay: false,
-              viewportFraction: 0.8,
-              enableInfiniteScroll: false,
-              disableCenter: true,
-              initialPage: 0,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  // Fortæller flutter at nyt data er passeret og skal rebuild hele programmet.
-                  _currentIndex = index;
-                });
-              },
-            ),
-            items: _travelCards.map(
-              (data) {
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.asset(
-                          'assets/images/rejsekort.png',
-                          height: 250,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 20,
-                        right: 10,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 5,
-                            horizontal: 20,
+          Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: 170.0,
+                enlargeCenterPage: true,
+                autoPlay: false,
+                viewportFraction: 0.8,
+                enableInfiniteScroll: false,
+                disableCenter: true,
+                initialPage: 0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    // Fortæller flutter at nyt data er passeret og skal rebuild hele programmet.
+                    _currentIndex = index;
+                  });
+                },
+              ),
+              items: _travelCards.map(
+                (data) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            'assets/images/rejsekort.png',
+                            height: 250,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
                           ),
-                          child: Text(
-                            data.id,
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.black,
+                        ),
+                        Positioned(
+                          bottom: 20,
+                          right: 10,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 20,
                             ),
-                            softWrap: true,
-                            overflow: TextOverflow.fade,
+                            child: Text(
+                              data.id,
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black,
+                              ),
+                              softWrap: true,
+                              overflow: TextOverflow.fade,
+                            ),
                           ),
                         ),
-                      ),
-                      // Positioned(
-                      //   bottom: 110,
-                      //   right: 20,
-                      //   child: Container(
-                      //     padding: EdgeInsets.symmetric(
-                      //       vertical: 5,
-                      //       horizontal: 20,
-                      //     ),
-                      //     child: Text(
-                      //       data.type.toUpperCase(),
-                      //       style: TextStyle(
-                      //         fontSize: 18,
-                      //         color: Colors.blue.shade600,
-                      //         fontWeight: FontWeight.w700,
-                      //         letterSpacing: 1,
-                      //       ),
-                      //       softWrap: true,
-                      //       overflow: TextOverflow.fade,
-                      //     ),
-                      //   ),
-                      // )
-                    ],
-                  ),
-                );
-              },
-            ).toList(),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        DotsIndicator(
-          decorator: const DotsDecorator(
-            size: Size.square(5),
+          const SizedBox(
+            height: 10,
           ),
-          dotsCount: _travelCards.length,
-          position: _currentIndex.toDouble(),
-        ),
-      ],
+          DotsIndicator(
+            decorator: const DotsDecorator(
+              size: Size.square(5),
+            ),
+            dotsCount: _travelCards.length,
+            position: _currentIndex.toDouble(),
+          ),
+        ],
+      ),
     );
   }
 }
